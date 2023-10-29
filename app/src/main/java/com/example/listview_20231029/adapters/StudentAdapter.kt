16f92db5ccd703c51c10b005edf6407113a54a41
rhythmshare <randomcony@gmail.com>
2023-10-29 11:36:19 +0900

    Importing ListItem xml from Adapter class

--- app/src/main/java/com/example/listview_20231029/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listview_20231029/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.listview_20231029.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listview_20231029.R
 import com.example.listview_20231029.datas.StudentData
 
 class StudentAdapter(
@@ -9,5 +13,24 @@ class StudentAdapter(
     resId: Int,
     mList : ArrayList<StudentData>
 ) : ArrayAdapter<StudentData>(mContext, resId, mList){ // ArrayAdapter: 기본 생성자 지원 X. parameter를 넣어줘야 함
+
+//    커스텀으로 만든 xml을 => 코틀린단으로 끌어와주는 객체
+
+    val inf = LayoutInflater.from(mContext)
+
+//    완성한 inf를 이용해서 => xml을 가져와주는 함수
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
 
 }
