939edbbb992aad9ec4c7679a593a35b263b1b61a
rhythmshare <randomcony@gmail.com>
2023-10-29 11:19:26 +0900

    Create a StudentData class to manage variables related to students at once

---
+++ app/src/main/java/com/example/listview_20231029/datas/StudentData.kt
@@ -0,0 +1,8 @@
+package com.example.listview_20231029.datas
+
+class StudentData(  // StudentData의 생성자 변경 : 이름, 출생년도, 폰번호를 넣어서 생성하도록 함
+    name: String,
+    birthYear: Int,
+    phoneNum: String) {
+
+}
